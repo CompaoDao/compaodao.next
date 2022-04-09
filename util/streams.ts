f70ac0f1b66e7281f78953b5172c7bfd5ba5e846
uladzislau.radkevich <uladzislau.radkevich@revolut.com>
2022-04-09 18:41:05 +0200

    Hourly inflows/outflows aggregates

--- util/streams.ts
+++ util/streams.ts
@@ -4,35 +4,67 @@ const client = createClient({
   url: 'https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-mumbai',
 })
 
-type Period = {
-  start: bigint, // inclusive
-  end: bigint, // exclusive
-  sum: bigint
+type PeriodFlow = {
+  start: Date,
+  sum: bigint,
+  currency: string,
 }
 
-function isOverlaps(currentPeriod: Period, startedAtTimestamp: bigint, stoppedAtTimestamp: bigint) {
-  console.log("startedAtTimestamp", startedAtTimestamp);
-  console.log("stoppedAtTimestamp", stoppedAtTimestamp);
-  if (stoppedAtTimestamp === null) {
-    return startedAtTimestamp < currentPeriod.end;
+export const getHourlyOutflow = async (account: string) => {
+  let date = new Date();
+  date.setHours(date.getHours() - 11, 0, 0, 0);
+
+  const periods: PeriodFlow[] = [];
+  while (date <= new Date()) {
+    let periodEndDate = new Date(date);
+    periodEndDate.setHours(date.getHours() + 1);
+
+    periods.push({
+      start: new Date(date),
+      sum: await getSumForPeriod(account, date, periodEndDate, true),
+      currency: 'fDAIx'
+    });
+
+    date.setHours(date.getHours() + 1);
   }
 
-  return stoppedAtTimestamp >= currentPeriod.start && startedAtTimestamp < currentPeriod.end;
+  return periods;
 }
 
-function min(left: bigint, right: bigint) {
-  return left < right ? left : right;
+export const getHourlyInflow = async (account: string) => {
+  let date = new Date();
+  date.setHours(date.getHours() - 11, 0, 0, 0);
+
+  const periods: PeriodFlow[] = [];
+  while (date <= new Date()) {
+    let periodEndDate = new Date(date);
+    periodEndDate.setHours(date.getHours() + 1);
+
+    periods.push({
+      start: new Date(date),
+      sum: await getSumForPeriod(account, date, periodEndDate, false),
+      currency: 'fDAIx'
+    });
+
+    date.setHours(date.getHours() + 1);
+  }
+
+  return periods;
 }
 
-function max(left: bigint, right: bigint) {
-  return left > right ? left : right;
-}
+export const getSumForPeriod = async (account: string, start: Date, end: Date, outgoing: boolean) => {
+  const startInSeconds = Math.floor(start.getTime() / 1000);
+  const endInSeconds = Math.floor(end.getTime() / 1000);
 
-export const getOutgoingFlowByPeriod = async (sender: string, step: number, start: number, end = Date.now()) => {
-  const streamPeriods = `
+  const endedStreamPeriods = `
     {
       streamPeriods(
-        where: {sender: "${sender.toLowerCase()}"}
+        where: {
+          ${outgoing ? "sender" : "receiver"}: "${account.toLowerCase()}", 
+          startedAtTimestamp_lte: ${endInSeconds}
+          stoppedAtTimestamp_gt: ${startInSeconds}
+          token: "0x5d8b4c2554aeb7e86f387b4d6c00ac33499ed01f"
+        }
         orderBy: startedAtTimestamp
       ) {
         token {
@@ -44,31 +76,41 @@ export const getOutgoingFlowByPeriod = async (sender: string, step: number, star
       }
     }`
 
-  const result = await client.query(streamPeriods).toPromise()
+  let result = await client.query(endedStreamPeriods).toPromise();
+  const sumForEndedPeriods = result.data.streamPeriods
+    .map(sp => {
+      const activeSeconds = Math.min(endInSeconds, Number(sp.stoppedAtTimestamp)) - Math.max(startInSeconds, Number(sp.startedAtTimestamp));
+      return BigInt(sp.flowRate) * BigInt(activeSeconds);
+    })
+    .reduce((prevValue: bigint, currentValue: bigint) => prevValue + currentValue, BigInt(0));
 
-  let periods: Period[] = [];
-  for (let i = start; i < end; i += step) {
-    const currentPeriod = {
-      start: BigInt(i),
-      end: BigInt(i + step),
-      sum: BigInt(0)
-    };
-
-    for (let j = 0; j < result.data.streamPeriods.length; j++) {
-      const sp = result.data.streamPeriods[j];
-
-      if (isOverlaps(currentPeriod, sp.startedAtTimestamp, sp.stoppedAtTimestamp)) {
-        const activeSeconds = min(currentPeriod.end, sp.stoppedAtTimestamp) - max(currentPeriod.start, sp.startedAtTimestamp);
-        currentPeriod.sum += sp.flowRate * BigInt(activeSeconds);
+  const unfinishedStreamPeriods = `
+    {
+      streamPeriods(
+        where: {
+          ${outgoing ? "sender" : "receiver"}: "${account.toLowerCase()}", 
+          startedAtTimestamp_lte: ${endInSeconds}
+          stoppedAtTimestamp: null
+          token: "0x5d8b4c2554aeb7e86f387b4d6c00ac33499ed01f"
+        }
+        orderBy: startedAtTimestamp
+      ) {
+        token {
+          id
+        }
+        flowRate
+        startedAtTimestamp
+        stoppedAtTimestamp
       }
+    }`
 
-      if (sp.startedAtTimestamp > currentPeriod.end) {
-        break;
-      }
-    }
+  result = await client.query(unfinishedStreamPeriods).toPromise();
+  const sumForUnfinishedPeriods = result.data.streamPeriods
+    .map(sp => {
+      const activeSeconds = endInSeconds - Math.max(startInSeconds, Number(sp.startedAtTimestamp));
+      return BigInt(sp.flowRate) * BigInt(activeSeconds);
+    })
+    .reduce((prevValue: bigint, currentValue: bigint) => prevValue + currentValue, BigInt(0));
 
-    periods.push(currentPeriod);
-  }
-
-  console.log("Periods: ", periods);
+  return sumForEndedPeriods + sumForUnfinishedPeriods;
 }
